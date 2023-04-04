fa6309c434efdb013ff16a9491311dd08af4e2b5
olgaodc <olgaodincova@gmail.com>
2023-04-05 00:49:47 +0300

    modal added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//DARK MODE
 const changeMode = document.getElementById('light-mode-button');
 changeMode.addEventListener('click', () => {
     let element = document.body;
@@ -6,6 +7,8 @@ changeMode.addEventListener('click', () => {
 
 
 
+
+//HAMBURGER MENU
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navigationMenu = document.querySelector('.navigation-list');
 
@@ -21,7 +24,47 @@ document.querySelectorAll('.navigation-item').forEach(n => n.addEventListener('c
 }))
 
 
+// NEVEIKIA.uzdarymas paspaudziant ant ekrano
+//window.addEventListener("click", (event) => {
+//     if (event.target == navigationMenu) {
+//         navigationMenu.style.display = "none";
+//     }
+// });
 
+
+
+//MODAL
+document.querySelector('.button').addEventListener('click', () => {
+    document.querySelector('#modal').classList.add('active');
+});
+    
+    
+document.querySelector('.modal-content .close').addEventListener('click', () => {
+    document.querySelector('#modal').classList.remove('active');
+});
+
+
+  // const openModalButton = document.getElementById("show-modal");
+  // const modal = document.getElementByClassName("subscribe-modal");
+  // const close = document.getElementsByClassName("close-btn")[0];
+  
+  // openModalButton.addEventListener("click", () => {
+  //   modal.style.display = "block";
+  // });
+  
+  // close.addEventListener("click", () => {
+  //   modal.style.display = "none";
+  // });
+  
+  // window.addEventListener("click", (event) => {
+  //   if (event.target == modal) {
+  //     modal.style.display = "none";
+  //   }
+  // });
+
+
+
+//SLIDE DOWN
 const heroBoxLink = document.querySelector('.hero-box-link');
 const launchingSection = document.getElementById('launching');
 
